8634629e043043e3cee63870b3b56243651fe6fc
Thomas Kirkland <thomasckirkland@gmail.com>
2019-03-12 11:08:47 -0600

    updated multiple coponents

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Container, Col, Image } from 'react-bootstrap'
+import './About.css'
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        ABOUT PAGE
+        <Image src="assets/USETHISHEADERDepositphotos_131701088_xl-2015 Cropped.jpg" className="header-image" />
+        <Container-Fluid>
+          <Col xs={12} sm={8} smOffset={2}>
+            <Image src="/Users/thomaskirkland/g106/treehouse-capstone/public/assets/USETHISboard-carpenter-carpentry-271628 Cropped.jpg" className="about-profile-pic" />
+            <h3>Our Employees</h3>
+            <p>If you overwork it you become a cloud killer. There's nothing worse than a cloud killer. Zip. That easy. Just think about these things in your mind and drop em' on canvas. We need a shadow side and a highlight side. Don't kill all your dark areas - you need them to show the light. Just relax and let it flow. That easy.</p>
+            <p>See how easy it is to create a little tree right in your world. I really recommend you use odorless thinner or your spouse is gonna run you right out into the yard and you'll be working by yourself. Very easy to work these to death. Making all those little fluffies that live in the clouds.</p>
+
+
+          </Col>
+        </Container-Fluid>
       </div>
     )
   }
--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,32 +1,74 @@
 import React, { Component } from 'react'
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { NavDropdown, Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './CustomNavbar.css';
 
-export default class CustomNavbar extends Component {
-  render() {
+class CustomNavbar extends Component {
+
+  render () {
     return (
-      <Navbar default collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/"></Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
-            <NavItem eventKey={1} componentClass={Link} href="/" to="/">
+      // <Navbar default collapseOnSelect>
+      //   <Navbar.Header>
+      //     <Navbar.Brand>
+      //       <Link to="/">Treehouse</Link>
+      //      </Navbar.Brand>
+      //       <Navbar.Toggle />
+      //       </Navbar.Header>
+      //        <Navbar.Collapse>
+      //      <Nav pullRight>
+      //       <NavItem eventKey={1} componentClass={Link} href="/" to="/">
+      //         Home
+      //       </NavItem>
+      //       <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
+      //         About
+      //       </NavItem>
+      //       <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
+      //         News
+      //       </NavItem>
+      //     </Nav>
+      //   </Navbar.CollapseOnSelect>
+      // </Navbar>
+
+<Navbar collapseOnSelect expand="lg" bg="light" variant="dark">
+  {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+  <Navbar.Brand eventKey={1} componentClass={Link} href="/" to="/">
+        Home
+      </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+{/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown"> */}
+
+        <NavItem eventKey={1} componentClass={Link} href="/" to="/">
               Home
+             </NavItem>
+             <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
+                About
             </NavItem>
-            <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
-              About
-            </NavItem>
-            <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
-              News
-            </NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+             <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
+                News
+             </NavItem>
+
+
+        {/* <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
+        <NavDropdown.Divider />
+        {/* <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown> */}
+     </Nav>
+      {/* <Nav> */}
+      {/* <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+    </Nav.Link>
+    </Nav> */}
+  </Navbar.Collapse>
+</Navbar>
     )
   }
 }
+
+export default CustomNavbar
